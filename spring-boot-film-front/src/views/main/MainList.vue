7f2ca3bbdbc8454eff5790ce503d68910a9e7b3b
fanjin <3502669452@qq.com>
2024-01-11 18:45:20 +0800

    修改布局

--- spring-boot-film-front/src/views/main/MainList.vue
+++ spring-boot-film-front/src/views/main/MainList.vue
@@ -9,9 +9,13 @@
        <div class="text">
 
          <div  class="title" style="display: block;">{{item.name}}</div>
-
+        <div  class="actor" style="display: block;">{{item.actor+'        '+item.region}}</div>
+         <div  class="type" style="display: block;">{{item.actor+'        '+item.region}}</div>
          <div  class="intro" style="display: block;">{{item.intro}}</div>
        </div>
+
+       <el-button type="success">查看详情</el-button>
+
 
 
 
@@ -40,20 +44,43 @@ display: flex;
   flex-direction: column;
   }
 .title{
-
+height:20px;
 display: flex;
 font-size: 20px; /* 根据需要调整字体大小 */
     font-weight: bold; /* 字体加粗 */
 margin-top:-700px;
 
 }
+.actor{
+height:10px;
+display: flex;
+font-size: 19px; /* 根据需要调整字体大小 */
+    font-weight: bold; /* 字体加粗 */
+margin-top:10px;
 
+}
+.type{
+height:10px;
+display: flex;
+font-size: 19px; /* 根据需要调整字体大小 */
+    font-weight: bold; /* 字体加粗 */
+margin-top:10px;
+
+}
 .intro{
+
 font-size: 18px;
 display: flex;
-margin-top:-700px;
+margin-top:450px;
+margin-left:350px;
+width: 600px; /* 你可以根据需要调整这个值 */
+  word-wrap: break-word;
+  line-height: 1.2;
 }
-
+.el-button{
+margin-top:-900px;
+margin-left:auto;
+}
 </style>
 <script>
 import axios from 'axios';
