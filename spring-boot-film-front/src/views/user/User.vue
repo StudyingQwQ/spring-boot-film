670c5818dd64cf136f783bbb5e815c426a11fdd7
StudyingQwQ <2703001087@qq.com>
2024-01-11 22:57:21 +0800

    后端登录返回值修改和获取用户信息

--- spring-boot-film-front/src/views/user/User.vue
+++ spring-boot-film-front/src/views/user/User.vue
@@ -3,9 +3,11 @@
   <div style="font-size: 14px; color: grey">邮箱:{{ email }}</div>
   <div style="font-size: 14px; color: grey">用户权限:{{ userRole }}</div>
   <div style="font-size: 14px; color: grey">积分:{{ points }}</div>
+  <el-button @click="router.push('/index')">跳转测试</el-button>
 </template>
 
 <script setup>
+import router from "@/router";
 const username = localStorage.getItem('username')
 const email = localStorage.getItem('email')
 const userRole = localStorage.getItem('role')
