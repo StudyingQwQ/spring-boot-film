2c2e7092c1389c41ea1f4b628f9ced0a90a139f9
StudyingQwQ <2703001087@qq.com>
2024-01-11 17:57:34 +0800

    完成普通用户与管理员用户界面初步隔离

--- spring-boot-film-front/src/router/index.js
+++ spring-boot-film-front/src/router/index.js
@@ -1,26 +1,26 @@
-import { createRouter, createWebHistory } from "vue-router";
-import {adminAuth, unauthorized} from "@/net/index.js";
+import { createRouter, createWebHistory } from 'vue-router'
+import { unauthorized } from "@/net";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: "/",
-            name: "welcome",
-            component: () => import("@/views/WelcomeView.vue"),
+            path: '/',
+            name: 'welcome',
+            component: () => import('@/views/WelcomeView.vue'),
             children: [
                 {
-                    path: "",
-                    name: "welcome-login",
-                    component: () => import("@/views/welcome/LoginPage.vue"),
-                },{
+                    path: '',
+                    name: 'welcome-login',
+                    component: () => import('@/views/welcome/LoginPage.vue')
+                }, {
                     path: 'register',
                     name: 'welcome-register',
-                    component: () => import("@/views/welcome/RegisterPage.vue")
-                },{
+                    component: () => import('@/views/welcome/RegisterPage.vue')
+                }, {
                     path: 'forget',
                     name: 'welcome-forget',
-                    component: () => import("@/views/welcome/ForgetPage.vue")
+                    component: () => import('@/views/welcome/ForgetPage.vue')
                 }
             ]
         }, {
@@ -53,18 +53,16 @@ const router = createRouter({
         },
     ],
 })
-//路由前置守卫
+
 router.beforeEach((to, from, next) => {
     const isUnauthorized = unauthorized()
-    const admin = adminAuth()
     if(to.name.startsWith('welcome') && !isUnauthorized) {
         next('/index')
     } else if(to.fullPath.startsWith('/index') && isUnauthorized) {
         next('/')
-    } else if(to.fullPath.startsWith('/admin') && isUnauthorized) {
-        next('/')
-    } else {
+    }  else {
         next()
     }
 })
+
 export default router
